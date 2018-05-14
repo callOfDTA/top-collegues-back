85f31d5446cfa28a590dd0f71980788789888101
Clement.Renazeau <clement.renazeau@gmail.com>
2018-05-14 16:47:02 +0200

    ajout des services get collegues et patch score

---
+++ src/main/java/dev/top/entities/Action.java
@@ -0,0 +1,15 @@
+package dev.top.entities;
+
+public class Action {
+
+	private Avis action;
+
+	public Avis getAction() {
+		return action;
+	}
+
+	public void setAction(Avis action) {
+		this.action = action;
+	}
+
+}
