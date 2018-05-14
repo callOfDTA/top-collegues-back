9aea380ee9b893339a6a37c8727c2130190795b2
Maxime.DANIEL <m.daniel2009@hotmail.fr>
2018-05-14 16:17:57 +0200

    Service Backend corection crossorigin

--- src/main/java/dev/top/controller/CollegueCtrl.java
+++ src/main/java/dev/top/controller/CollegueCtrl.java
@@ -3,6 +3,7 @@ package dev.top.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -16,6 +17,7 @@ import dev.top.repos.CollegueRepo;
 import dev.top.service.ActionService;
 
 @RestController()
+@CrossOrigin
 @RequestMapping("/Collegues")
 public class CollegueCtrl {
 
