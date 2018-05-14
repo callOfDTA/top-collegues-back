bf50beb8b12bc10627d17b8a5fc4eb8c6ac7e2e2
Maxime.DANIEL <m.daniel2009@hotmail.fr>
2018-05-14 14:25:03 +0200

    Service Backend

---
+++ src/main/java/dev/top/entities/ActionIhm.java
@@ -0,0 +1,18 @@
+package dev.top.entities;
+
+public class ActionIhm {
+
+	private ActionEnum action;
+
+	public ActionIhm() {
+	}
+
+	public ActionEnum getAction() {
+		return action;
+	}
+
+	public void setAction(ActionEnum action) {
+		this.action = action;
+	}
+
+}
